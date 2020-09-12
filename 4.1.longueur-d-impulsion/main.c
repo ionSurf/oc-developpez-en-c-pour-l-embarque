d70d898c87787cc5e1f7a05cf1433798b07c573f
ionsurf <jprieto@LT-5CG9324RRY.corp.nexient.com>
2020-09-12 19:28:45 -0400

    PWM update

--- 4.1.longueur-d-impulsion/main.c
+++ 4.1.longueur-d-impulsion/main.c
@@ -32,22 +32,23 @@ void configure_timer2_with_IT(){
     NVIC->IP[28] |= (7 << 4);
 }
 
-int main(void) {
-
+int main (void)
+{
     configure_gpio_pa6_alternate_push_pull();
-    configure_pwm_ch1_20khz( TIM3 );
-    set_pulse_percentage( TIM3, 0x100 );
-		configure_timer2_with_IT();
-    start_timer( TIM3 );
-		start_timer( TIM2 );
-
-    while(1);
+    configure_pwm_ch1_20khz(TIM3);
+    set_pulse_percentage(TIM3, 0x100);
+    configure_timer2_with_IT();
+    start_timer(TIM3);
+    start_timer(TIM2);
+    while(1)
+	{
+	}
     return 0;
 }
 
 void TIM2_IRQHandler(void) { 
     static int pulse = 0; 
-	TIM3->SR &= ~TIM_SR_UIF;
+	TIM2->SR &= ~TIM_SR_UIF;
     pulse = (pulse + 5) % 101;
 	set_pulse_percentage(TIM3, pulse);
 }
